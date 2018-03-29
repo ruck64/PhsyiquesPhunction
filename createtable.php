2bdd342351636b7d5477101ea45ad782ab58dab1
ruck64 <rluther11@yahoo.com>
2018-03-29 13:49:17 -0600

    user name not right or password is bad

--- createtable.php
+++ createtable.php
@@ -12,7 +12,7 @@ $password = "b8fa5f57";
 //  }
 
     try {
-      $conn = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_3e6dc0754d58604','$username' ,'$password');
+      $conn = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_3e6dc0754d58604','bb2501c58a8034' ,'b8fa5f57');
   //    $this->logger->logDebug("Established a database connection.");
       return $conn;
     } catch (Exception $e) {
