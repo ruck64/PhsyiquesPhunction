960b1925510449d44e787c79ca76d10739eec0ae
ruck64 <rluther11@yahoo.com>
2018-03-30 11:06:21 -0600

    adding login stuff

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 	<html>
 		<head>
 			<link rel="stylesheet" type="text/css" href="stylesheet.css">
+			<link rel= "stylesheet" type  = "text/css" href = "login.css">
 			<title> Login/ Sign up </title>
 		</head>
 	<body>
@@ -30,7 +31,69 @@
 		</div>
 		
 	</body>	
-<p class ="button"> <a href = "user.html"><input type="button" value="Your profile Dude"></p>
+<div class="form">
+      
+      <ul class="tab-group">
+        <li class="tab active"><a href="#signup">Sign Up</a></li>
+        <li class="tab"><a href="#login">Log In</a></li>
+      </ul>
+      
+      <div class="tab-content">
+        <div id="signup">   
+          <h1>Sign Up for Free</h1>
+          
+          <form action="/" method="post">
+
+          <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              Set A Password<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off"/>
+          </div>
+          
+          <button type="submit" class="button button-block"/>Get Started</button>
+          
+          </form>
+
+        </div>
+        
+        <div id="login">   
+          <h1>Welcome Back!</h1>
+          
+          <form action="/" method="post">
+          
+            <div class="field-wrap">
+            <label>
+              Email Address<span class="req">*</span>
+            </label>
+            <input type="email"required autocomplete="off"/>
+          </div>
+          
+          <div class="field-wrap">
+            <label>
+              Password<span class="req">*</span>
+            </label>
+            <input type="password"required autocomplete="off"/>
+          </div>
+          
+          <p class="forgot"><a href="#">Forgot Password?</a></p>
+          
+          <button class="button button-block"/>Log In</button>
+          
+          </form>
+
+        </div>
+        
+      </div><!-- tab-content -->
+      
+</div> <!-- /form -->
 
 <div class = "footer">
 	This is the footer
