3b8978f25ba759ffd8834f610f808fdf66b06cf1
ruck64 <rluther11@yahoo.com>
2018-03-30 11:39:27 -0600

    added dipslay name

--- login.php
+++ login.php
@@ -45,19 +45,26 @@
           <h1>Sign Up for Free</h1>
           
           <form action="/" method="post">
+		  
+		  <div class = "field-wrap">
+			<label>
+				Display Name<span class="req">*</span>
+			</label>
+			<input type="text"required autocomplete="on"/>
+		  </div>
 
           <div class="field-wrap">
             <label>
               Email Address<span class="req">*</span>
             </label>
-            <input type="email"required autocomplete="off"/>
+            <input type="email"required autocomplete="on"/>
           </div>
           
           <div class="field-wrap">
             <label>
               Set A Password<span class="req">*</span>
             </label>
-            <input type="password"required autocomplete="off"/>
+            <input type="password"required autocomplete="on"/>
           </div>
           
           <button type="submit" class="button button-block"/>Get Started</button>
