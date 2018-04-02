66f668c51a3be4a35ba2a2562ee1d1d0874bc14e
ruck64 <rluther11@yahoo.com>
2018-04-01 18:27:27 -0600

    dealing with login forms and css

--- login.php
+++ login.php
@@ -6,9 +6,6 @@
 	require_once "createtable.php";
 	require_once "comments.php";
 	require_once "Users.php";
-	$Users = new Users();
-	
-	$save = $Users->saveUser();
 	
 	?>
 	
@@ -61,163 +58,142 @@
 	<li class = "subMenu"><a class = "subMenu"  href = "contact.php">Contact</a></li> 
 	<li class = "subMenu"><a class = "subMenu"  href = "login.php">Sign Up/Login</a></li> 
 	</ul>
-		'
-<div class="cont_principal">
-
-  <div class="cont_centrar">
-  <div class="cont_login">
-    <form action="handlercreate.php" method="POST" >
-    <div class="cont_tabs_login">
-      <ul class='ul_tabs'>
-        <li class="active"><a href="#" onclick="sign_in()">SIGN IN</a>
-        <span class="linea_bajo_nom"></span>
-        </li>
-        <li><a href="#up" onclick="sign_up()">SIGN UP</a><span class="linea_bajo_nom"></span>
-        </li>
-      </ul>
-      </div>
-  <div class="cont_text_inputs">
-      <input value = "<?php echo isset($presets['display_name']) ? $presets['display_name'] : ''; ?>" type="text" class="input_form_sign " placeholder="DISPLAY NAME" id = "display_name" name="display_name" />
-    
-    <input type="text" class="input_form_sign d_block active_inp" placeholder="EMAIL" name="emauil_us" />
-
-    <input type="password" class="input_form_sign d_block  active_inp" placeholder="PASSWORD" name="pass_us" />  
-   <input type="password" class="input_form_sign" placeholder="CONFIRM PASSWORD" name="conf_pass_us" />
-    
-    <a href="#" class="link_forgot_pass d_block" >Forgot Password ?</a>    
-      </div>
-<div class="cont_btn">
-     <button class="btn_sign" type="submit" value="Submit">SIGN IN</button>
-      
-      </div>
-      
-    </form>
-    </div>
-    
-  </div>
-  
-
-</div>
-
-
-
-<script>
-function sign_up(){
-  var inputs = document.querySelectorAll('.input_form_sign');
-document.querySelectorAll('.ul_tabs > li')[0].className=""; 
-document.querySelectorAll('.ul_tabs > li')[1].className="active"; 
-  
-  for(var i = 0; i < inputs.length ; i++  ) {
-if(i == 2  ){
-
-}else{  
-document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
-}
-} 
-
-setTimeout( function(){
-for(var d = 0; d < inputs.length ; d++  ) {
- document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block active_inp";  
-   }
-
-
- },100 );
- document.querySelector('.link_forgot_pass').style.opacity = "0";
-   document.querySelector('.link_forgot_pass').style.top = "-5px";
-   document.querySelector('.btn_sign').innerHTML = "SIGN UP";    
-  setTimeout(function(){
-
- document.querySelector('.terms_and_cons').style.opacity = "1";
-  document.querySelector('.terms_and_cons').style.top = "5px";
- 
-  },500);
-  setTimeout(function(){
-    document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_none";
- document.querySelector('.terms_and_cons').className = "terms_and_cons d_block";
-  },450);
-
-}
-
-
-
-function sign_in(){
-  var inputs = document.querySelectorAll('.input_form_sign');
-document.querySelectorAll('.ul_tabs > li')[0].className = "active"; 
-document.querySelectorAll('.ul_tabs > li')[1].className = ""; 
-  
-  for(var i = 0; i < inputs.length ; i++  ) {
-switch(i) {
-    case 1:
- console.log(inputs[i].name);
-        break;
-    case 2:
- console.log(inputs[i].name);
-    default: 
-document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
-}
-} 
-
-setTimeout( function(){
-for(var d = 0; d < inputs.length ; d++  ) {
-switch(d) {
-    case 1:
- console.log(inputs[d].name);
-        break;
-    case 2:
- console.log(inputs[d].name);
-
-    default:
- document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block";  
- document.querySelectorAll('.input_form_sign')[2].className = "input_form_sign d_block active_inp";  
-
-   }
-  }
- },100 );
-
- document.querySelector('.terms_and_cons').style.opacity = "0";
-  document.querySelector('.terms_and_cons').style.top = "-5px";
-
-  setTimeout(function(){
- document.querySelector('.terms_and_cons').className = "terms_and_cons d_none"; 
-document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_block";
-
- },500);
-
-  setTimeout(function(){
-
- document.querySelector('.link_forgot_pass').style.opacity = "1";
-   document.querySelector('.link_forgot_pass').style.top = "5px";
-    
-
-for(var d = 0; d < inputs.length ; d++  ) {
-
-switch(d) {
-    case 1:
- console.log(inputs[d].name);
-        break;
-    case 2:
- console.log(inputs[d].name);
-
-         break;
-    default:
- document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign";  
-}
-  }
-   },1500);
-   document.querySelector('.btn_sign').innerHTML = "SIGN IN";    
-}
-
-
-window.onload =function(){
-  document.querySelector('.cont_centrar').className = "cont_centrar cent_active";
-
-}
-
-</script>
 	
-<div class = "footer">
-	This is the footer
-		</div>	
+	<?php 
+	echo "htmlspecialchars($user[display_name]";
+		
+	<div class="cont_principal">
+
+		<div class="cont_centrar">
+			<div class="cont_login">
+				<form action="handlercreate.php" method="POST" enctype="multipart/form-data" >
+					<div class="cont_tabs_login">
+						<ul class='ul_tabs'>
+							<li class="active"><a href="#" onclick="sign_in()">SIGN IN</a><span class="linea_bajo_nom"></span></li>
+							<li><a href="#up" onclick="sign_up()">SIGN UP</a><span class="linea_bajo_nom"></span></li>
+						</ul>
+					</div>
+					<div class="cont_text_inputs">
+						<input value = "<?php echo isset($presets['display_name']) ? $presets['display_name'] : ''; ?>" type="text" class="input_form_sign " placeholder="DISPLAY NAME" id = "display_name" name="display_name" />
+						<input type="text" class="input_form_sign d_block active_inp" placeholder="EMAIL" name="emauil_us" />
+						<input type="password" class="input_form_sign d_block  active_inp" placeholder="PASSWORD" name="pass_us" />  
+						<input type="password" class="input_form_sign" placeholder="CONFIRM PASSWORD" name="conf_pass_us" /><a href="#" class="link_forgot_pass d_block" >Forgot Password ?</a>    
+					</div>
+					<div class="cont_btn">
+						<button class="btn_sign" type="submit" value="Submit">SIGN IN</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<script>
+		function sign_up(){
+		var inputs = document.querySelectorAll('.input_form_sign');
+		document.querySelectorAll('.ul_tabs > li')[0].className=""; 
+		document.querySelectorAll('.ul_tabs > li')[1].className="active"; 
+		for(var i = 0; i < inputs.length ; i++  ) {
+		if(i == 2  ){
+
+			}else{  
+			document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
+			}
+		} 
+
+		setTimeout( function(){
+		for(var d = 0; d < inputs.length ; d++  ) {
+			document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block active_inp";  
+		}
+		},100 );
+		document.querySelector('.link_forgot_pass').style.opacity = "0";
+		document.querySelector('.link_forgot_pass').style.top = "-5px";
+		document.querySelector('.btn_sign').innerHTML = "SIGN UP";    
+  
+		setTimeout(function(){
+			document.querySelector('.terms_and_cons').style.opacity = "1";
+			document.querySelector('.terms_and_cons').style.top = "5px";
+		},500);
+		
+		setTimeout(function(){
+		document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_none";
+		document.querySelector('.terms_and_cons').className = "terms_and_cons d_block";
+		},450);
+
+	}
+
+
+	function sign_in(){
+		var inputs = document.querySelectorAll('.input_form_sign');
+		document.querySelectorAll('.ul_tabs > li')[0].className = "active"; 
+		document.querySelectorAll('.ul_tabs > li')[1].className = ""; 
+		for(var i = 0; i < inputs.length ; i++  ) {
+		switch(i) {
+			case 1:
+				console.log(inputs[i].name);
+			break;
+			case 2:
+				console.log(inputs[i].name);
+			default: 
+				document.querySelectorAll('.input_form_sign')[i].className = "input_form_sign d_block";
+		}
+	} 
+
+	setTimeout( function(){
+		for(var d = 0; d < inputs.length ; d++  ) {
+			switch(d) {
+				case 1:
+					console.log(inputs[d].name);
+				break;
+			case 2:
+				console.log(inputs[d].name);
+
+			default:
+				document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign d_block";  
+				document.querySelectorAll('.input_form_sign')[2].className = "input_form_sign d_block active_inp";  
+
+			}
+		}
+	},100 );
+
+	document.querySelector('.terms_and_cons').style.opacity = "0";
+	document.querySelector('.terms_and_cons').style.top = "-5px";
+
+	setTimeout(function(){
+		document.querySelector('.terms_and_cons').className = "terms_and_cons d_none"; 
+		document.querySelector('.link_forgot_pass').className = "link_forgot_pass d_block";
+
+	},500);
+
+	setTimeout(function(){
+		document.querySelector('.link_forgot_pass').style.opacity = "1";
+		document.querySelector('.link_forgot_pass').style.top = "5px";
+		for(var d = 0; d < inputs.length ; d++  ) {
+			switch(d) {
+				case 1:
+					console.log(inputs[d].name);
+				break;
+				case 2:
+					console.log(inputs[d].name);
+				break;
+				default:
+					document.querySelectorAll('.input_form_sign')[d].className = "input_form_sign";  
+			}
+		}
+	},1500);
+	document.querySelector('.btn_sign').innerHTML = "SIGN IN";    
+	}
+
+
+	window.onload =function(){
+	document.querySelector('.cont_centrar').className = "cont_centrar cent_active";
+
+	}
+
+	</script>
+	
+	<div class = "footer">
+		This is the footer
+	</div>	
 		
 	</body>		
 
