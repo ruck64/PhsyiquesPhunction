e94b25923e64ae36786eca9ee1717cde63930a9b
ruck64 <rluther11@yahoo.com>
2018-04-02 19:06:14 -0600

    dealing with login forms and css

--- handlercreate.php
+++ handlercreate.php
@@ -7,7 +7,7 @@
 	$Users = new Users();
 	
 	try {
-		require_once "dbconfig.php"
+		require_once "dbconfig.php";
 		$conn = new PDO('mysql:host=$host;dbname=$db',$username,$password);
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (PDOException $e){
