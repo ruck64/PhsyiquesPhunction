4fcb8bfc3580c6467845c06d06417b1aae1a21a7
ruck64 <rluther11@yahoo.com>
2018-04-09 17:57:23 -0600

    dealing with login forms and css

--- userpage.php
+++ userpage.php
@@ -1,6 +1,5 @@
 <?php 
 	session_start();
-	echo $_SESSION['id'];
 	
 	require_once "comments.php";
 	require_once "Users.php";
@@ -43,7 +42,6 @@
 	</ul>
 		</div>
       <?php
-    session_start();
     echo "<h3> PHP List All Session Variables</h3>";
     foreach ($_SESSION as $key=>$val)
     echo $key." ".$val."<br/>";
