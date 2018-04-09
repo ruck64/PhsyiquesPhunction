cda9145168f8357916ec9dc33d1dbc4d07c468c8
ruck64 <rluther11@yahoo.com>
2018-04-09 17:44:50 -0600

    dealing with login forms and css

--- handlerlogin.php
+++ handlerlogin.php
@@ -38,6 +38,7 @@
 		if($query->fetchColumn() === $_POST['password'] && $valid) //better to hash it
 		{
 			$display =  $conn->prepare("SELECT display_name FROM users WHERE email=?");
+			$display = setFetchMode(PDO::FETCH::ASSOC);
 			$display->execute();
 			$_SESSION['display_name'] = $display->fetchColumn();
 			setcookie("display_name","display_name");
