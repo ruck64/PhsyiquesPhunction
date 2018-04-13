2dcca661a05549997d9b61106b286607b34f1458
ruck64 <rluther11@yahoo.com>
2018-04-13 10:54:39 -0600

    fixing small thing

--- handlerlogin.php
+++ handlerlogin.php
@@ -40,7 +40,7 @@
 			$getId = $conn->prepare("SELECT id FROM users Where email=:email");
 			$getId->execute(array(':email' => $email));
 			$user = $getId->fetchAll(PDO::FETCH_ASSOC);
-			$_SESSION['id'] = $user['id'];;
+			$_SESSION['id'] = $user['id'];
 			$_SESSION['display_name'] = $user['display_name'];
 			setcookie("display_name","display_name");
 			$messages[] = "login successful";
