b1804de03cddea99f205cd0b98cd99ee8c84d448
ruck64 <rluther11@yahoo.com>
2018-04-13 15:03:23 -0600

    fixing small thing

---
+++ userinfo.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<?php 
+	session_start();
+	
+	require_once "comments.php";
+	require_once "Users.php";
+	
+	if(!isset($_SESSION['id'])) {
+		header("Location:login.php");
+		exit;
+	}
+?>
+  <html>
+    <head>
+      <title> User </title>
+      <link rel="stylesheet" type="text/css" href="stylesheet.css">
+	  <link rel="stylesheet" type="text/css" href="user.css">
+    </head>
+
+    <h1 class = "title"> User Info </h1>
+
+ 	<div class ="handles">
+	<ul class="handles">
+	<li class="handles"><a class="handles" href="https://www.twitter.com"> <img src="twitter.png" width=100px; height=100px;></a></li> 
+	<li class="handles"><a class="handles" href="https://www.snapchat.com"> <img src="snapchat.png" width=100px; height=100px;></a></li>
+	<li class="handles"><a class="handles" href="https://www.instagram.com"> <img src="instagram.png" width=100px; height=100px;></a></li>
+	<li class="handles"><a class="handles" href="https://www.twitch.com"> <img src="twitch.png" width=100px; height=100px;></a></li>
+	<li class="handles"><a class="handles" href="https://www.youtube.com"> <img src="youtube.png" width=100px; height=100px;></a></li>
+	</ul>
+	</div>
+	
+	<div class="subMenu"> 
+	<ul class = "subMenu">
+	<li class = "subMenu"><a class = "subMenu"  href = "https://ruck64.herokuapp.com/home.php">Home</a></li>
+	<li class = "subMenu"><a class = "subMenu"  href = "myStory.php">My Story</a></li>  
+	<li class = "subMenu"><a class = "subMenu"  href = "purpose.php">Purpose of PhsyquiesPhunction</a></li> 
+	<li class = "subMenu"><a class = "subMenu"  href = "regimens.php">Regimens/Diet</a></li> 
+	<li class = "subMenu"><a class = "subMenu"  href = "https://www.twitch.tv/">Twitch</a></li> 
+	<li class = "subMenu"><a class = "subMenu"  href = "contact.">Contact</a></li> 
+	<li class = "subMenu"><?php if(isset($_SESSION['id'])){ ?> <a class="subMenu" href="userpage.php">Your Page</a>
+	<li class = "subMenu"><a class = "subMenu" href="logout.php">Logout</a>
+		<?php }else{ ?>
+		<a class="subMenu" href="login.php">Sign Up/Login</a>
+		<?php } ?>
+	</li> 
+	</ul>
+		</div>
+
+      <h2> <?php echo $_SESSION['display_name'] . " info" ?> </h2>
+
+      <div id='parent_div_1'>
+		<p class="child_div_1"> Name: </p>
+		<p class="child_div_1"> Age: </p>
+		<p class="child_div_1"> Weight: </p>
+		<p class="child_div_1"> Body Type: </p>
+	  
+	  <div id = 'parent_div_2'>
+		<p class="shadow">Welcome User</p>
+		<p class ="child_div_1"> <a href = "userinfo.php"><input type="button" value="User Info"></p>
+		<p class ="child_div_1"> <a href = "userpics.php"><input type="button" value="Your pics"></p>
+		<p class ="child_div_1"> <a href = "editinfo.php"><input type="button" value="Edit Your Info"</p>
+	  </div>
+
+	  <div class = "footer">
+	This is the footer
+		</div>
+	  
+  </html>
