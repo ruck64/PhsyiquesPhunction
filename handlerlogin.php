64d5947715ec5dbd2faeab80b47cbb7f5b8d7d78
ruck64 <rluther11@yahoo.com>
2018-04-16 18:11:35 -0600

    safety an jquery and fonts and such

--- handlerlogin.php
+++ handlerlogin.php
@@ -43,7 +43,7 @@
 
 	$query = $conn->prepare("SELECT password FROM Users WHERE email='$email'");
 	$query->execute(array($_POST['email']));
-	echo "query " . $query;
+	$passcheck = $query->fetchColum();
 	
 	if(!isset($error) && $valid)
 	{
