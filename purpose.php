2278f6691fcc328d35f22236a3008303ab8868d5
ruck64 <rluther11@yahoo.com>
2018-04-17 12:39:43 -0600

    maybe

--- purpose.php
+++ purpose.php
@@ -34,7 +34,7 @@
 		</div>
 		
 	</body>	
-<p class="basic">  <img src ="funny.jpg" alt = "funny hahaahha" class = "center">
+<p class="basic">  <img src ="funny.jpg" alt = "funny hahaahha" class = "center" width = 348px; height = 390px;>
 
 </p>
 	<div class = "footer">
