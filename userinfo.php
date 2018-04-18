7f9243cdd174fbd9d2956f5cc70df06dd62648da
ruck64 <rluther11@yahoo.com>
2018-04-18 14:18:46 -0600

    maybe

--- userinfo.php
+++ userinfo.php
@@ -100,7 +100,7 @@
 							</select>
 						</li>
 					</ul>
-					<p class="submit"><input type="submit" value="Submit"></p>
+					<p class="select"><input type="submit" value="Submit"></p>
 				</li>
 			</ul>
 		</li>
