7f3d27d20110b8985d746f375690b780ee3f750d
ruck64 <rluther11@yahoo.com>
2018-04-18 16:32:07 -0600

    maybe

--- userinfo.php
+++ userinfo.php
@@ -6,7 +6,6 @@
 	
 		$conn = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_3e6dc0754d58604','bb2501c58a8034' ,'b8fa5f57');
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$this->logger->logDebug("Established a database connection.");
 		return $conn;
 	
 		if(!isset($_SESSION['id'])) {
